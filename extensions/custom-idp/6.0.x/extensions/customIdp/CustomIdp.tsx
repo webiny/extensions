1bddf6572859c7484794b7cda321972b9358f9f7
Pavel Denisjuk <pavel@webiny.com>
2026-03-06 12:41:51 +0100

    fix: custom idp env var

--- extensions/custom-idp/6.0.x/extensions/customIdp/CustomIdp.tsx
+++ extensions/custom-idp/6.0.x/extensions/customIdp/CustomIdp.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { EnvVar, Admin, Api } from "webiny/extensions";
+import { Infra, Admin, Api } from "webiny/extensions";
 
 export interface IdpProps {
     secretKey: string;
@@ -9,7 +9,7 @@ export const CustomIdp = (props: IdpProps) => {
     return (
         <>
             {/* Lambda vars */}
-            <EnvVar varName={"WEBINY_API_IDP_SECRET_KEY"} value={props.secretKey} />
+            <Infra.EnvVar varName={"WEBINY_API_IDP_SECRET_KEY"} value={props.secretKey} />
             {/* Api extensions */}
             <Api.Extension src={import.meta.dirname + "/api/CustomIdentityProvider.ts"} />
             {/* Admin extensions */}
