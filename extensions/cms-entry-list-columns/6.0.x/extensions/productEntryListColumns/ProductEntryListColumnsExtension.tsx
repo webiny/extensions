1c829469e272e08bb964131e92c901e71f0c9a74
adrians5j <adrian@webiny.com>
2026-03-16 17:19:47 +0100

    chore: update cms entry list columns extension structure and paths

---
+++ extensions/cms-entry-list-columns/6.0.x/extensions/productEntryListColumns/ProductEntryListColumnsExtension.tsx
@@ -0,0 +1,14 @@
+import { Api, Admin } from "webiny/extensions";
+
+export const ProductEntryListColumnsExtension = () => {
+    return (
+        <>
+            <Admin.Extension
+                src={"/extensions/productEntryListColumns/ProductEntryListColumns.tsx"}
+            />
+
+            {/* This model is registered just for demo purposes. */}
+            <Api.Extension src={"/extensions/productEntryListColumns/ProductModel.ts"} />
+        </>
+    );
+};
